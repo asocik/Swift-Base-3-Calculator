b29b28f5f061754023cf386bdc8a4ab98d5eefd2
Adam Socik <socikadam@gmail.com>
2015-04-28 21:07:06 -0500

    Initial Commit

---
+++ Base 3 Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Base 3 Calculator
+//
+//  Created by Adam Socik on 4/28/15.
+//  Copyright (c) 2015 Adam Socik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
